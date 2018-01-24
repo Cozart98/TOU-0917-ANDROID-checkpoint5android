cab3e0918f64fa134b5c48aa3e1059aa679fd75e
Cozart98 <chihaoui1098@gmail.com>
2018-01-24 10:21:18 +0100

    ajout log hasContent

--- app/src/main/java/map/android/com/wcstravel/MainActivity.java
+++ app/src/main/java/map/android/com/wcstravel/MainActivity.java
@@ -2,12 +2,36 @@ package map.android.com.wcstravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+	private String TAG = "Value";
+	DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ref.child("checkpoint5/students/cozart98").child("hasContent").addValueEventListener
+				(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				Log.d(TAG, "onDataChange: " + dataSnapshot.getValue(Boolean.class));
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError) {}
+		});
 	}
 }
